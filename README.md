1edeb971fb69f56204db9d262734d731836019a2
Iris <iclinde@xs4all.nl>
2021-05-17 14:39:55 +0200

    setting up files

---
+++ README.md
@@ -0,0 +1,2 @@
+this is a project from the odin project.
+the goal is to use module and factory to create a tic tac toe game.
---
+++ tic.js
@@ -0,0 +1,18 @@
+const gameBoard = (() => {
+    const board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"];
+
+    return board;
+  })();
+
+  const Player = (name, symbol) => {
+    
+  };
+  
+  const player1 = Player('player1', "X");
+  const player2 = Player('player2', "O");
+
+  const displayController = (() => {
+    const display = [];
+
+    return display;
+  })();
