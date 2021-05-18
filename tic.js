cdf789a82c396bee750f9663c8bef57f785d4734
Iris <iclinde@xs4all.nl>
2021-05-18 14:54:43 +0200

    worked showing the board and logic to play game

--- tic.js
+++ tic.js
@@ -1,11 +1,24 @@
 const gameBoard = (() => {
-    const board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"];
-
-    return board;
+    const board = ["X", "O", "O", "O", "X", "O", "O", "O", "X"];
+    function changeboard() {
+      document.getElementById("topleft").innerText = board[0];
+      document.getElementById("topmid").innerText = board[1];
+      document.getElementById("topright").innerText = board[2];
+      document.getElementById("midleft").innerText = board[3];
+      document.getElementById("midmid").innerText = board[4];
+      document.getElementById("midright").innerText = board[5];
+      document.getElementById("botleft").innerText = board[6];
+      document.getElementById("botmid").innerText = board[7];
+      document.getElementById("botright").innerText = board[8];
+    }
+    return changeboard();
+    
   })();
 
   const Player = (name, symbol) => {
-    
+    const getName  = () => name;
+    const getSymbol = () => symbol;
+    return {getName , getSymbol}
   };
   
   const player1 = Player('player1', "X");
@@ -13,6 +26,6 @@ const gameBoard = (() => {
 
   const displayController = (() => {
     const display = [];
-
-    return display;
+    const getActivePlayer = () => {}
+    return {display , getActivePlayer}
   })();
